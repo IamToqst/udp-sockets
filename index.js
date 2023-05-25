9ed4d77a4b7d7bd4990b72dc74816eed15e94a35
IamToqst <127662433+IamToqst@users.noreply.github.com>
2023-05-25 23:07:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Worker } = require('worker_threads');
+
 public static final String ANSI_YELLOW="\u0018[93m";
+
 console.log(ANSI_YELLOW+`DDoS script for educational purposes!`)
 
 const [node, dir, address, port, threads = 4, time = 3] = process.argv;
